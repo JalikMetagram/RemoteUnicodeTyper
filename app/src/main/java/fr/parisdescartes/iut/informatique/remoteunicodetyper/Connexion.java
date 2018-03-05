b7412ce426ea3d72ba6df5db861da178fa75cfd2
JALIK <jalik.metagram@gmail.com>
2018-03-05 10:10:26 +0100

    Maintenant on entre l'adresse IP par un Edit Text, c'est plus hardcoded

--- app/src/main/java/fr/parisdescartes/iut/informatique/remoteunicodetyper/Connexion.java
+++ app/src/main/java/fr/parisdescartes/iut/informatique/remoteunicodetyper/Connexion.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.net.Socket;
 
@@ -11,27 +14,38 @@ public class Connexion extends AppCompatActivity {
 
     private static final int PORT = 33555;
 
-    private static final String ADRESSE = "192.168.43.75";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
         Log.i("OH", "connexion");
-        Intent goMain = new Intent(Connexion.this, TestBoutons.class);
-        Intent goErreur = new Intent(Connexion.this, ErreurConnexion.class);
-        try{
-            connexionServeur client = new connexionServeur(ADRESSE, PORT);
-            client.execute(); //On doit déléguer les interractions Client-Serveur à un autre Thread
-            //Sinon on a : android.os.NetworkOnMainThreadException
-            client.get(); //Permet d'attendre la fin de l'execution
-            Socket cli = client.getSocket();
+        final Intent goMain = new Intent(Connexion.this, TestBoutons.class);
+        final Intent goErreur = new Intent(Connexion.this, ErreurConnexion.class);
 
-            //Lancer activité Main avec client en paramètre
-            TestBoutons.setClient(cli);
-            startActivity(goMain);
-        }catch(Exception e){
-            ErreurConnexion.setException(e);
-            startActivity(goErreur);
-        }
+        Button connexion = findViewById(R.id.conn);
+        final EditText adresse = findViewById(R.id.adress);
+
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    //On doit déléguer les interractions Client-Serveur à un autre
+                    //Sinon on a : android.os.NetworkOnMainThreadException
+                    connexionServeur client
+                            = new connexionServeur(adresse.getText().toString(), PORT);
+                    client.execute();
+
+                    client.get(); //Permet d'attendre la fin de l'execution
+                    Socket cli = client.getSocket();
+
+                    //Lancer activité Main avec client en paramètre
+                    TestBoutons.setClient(cli);
+                    startActivity(goMain);
+                }catch(Exception e){
+                    ErreurConnexion.setException(e);
+                    startActivity(goErreur);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/fr/parisdescartes/iut/informatique/remoteunicodetyper/TestBoutons.java
+++ app/src/main/java/fr/parisdescartes/iut/informatique/remoteunicodetyper/TestBoutons.java
@@ -2,12 +2,77 @@ package fr.parisdescartes.iut.informatique.remoteunicodetyper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.w3c.dom.Text;
+
+import java.net.Socket;
 
 public class TestBoutons extends AppCompatActivity {
 
+    private static Socket client = null;
+
+    public static void setClient(Socket cli){
+        client = cli;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_boutons);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_test_boutons);
+            Log.i("AH", "TestBoutons");
+            Button bouton_complexSet = findViewById(R.id.button8450);
+            Button bouton_woman = findViewById(R.id.buttonWoman);
+            Button bouton_brimestone = findViewById(R.id.buttonBrimestone);
+            Button bouton_nani = findViewById(R.id.buttonNANI);
+            //NANI : 20309
+            //WOMAN : 9792
+            //BRIMESTONE : 128783
+            bouton_complexSet.setText("COMPLEXE" /*(char) 8450*/);
+            bouton_woman.setText("FEMINISM");
+            bouton_nani.setText("OMAEWA_MO_SHINDERU");
+            bouton_brimestone.setText("BRIMESTONE");
+
+            bouton_complexSet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("envoie", "Bouton clic");
+                    envoieCharactère envoie = new envoieCharactère(client, 8450);
+                    envoie.execute();
+                }
+            });
+
+            bouton_woman.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("envoie", "Bouton clic");
+                    envoieCharactère envoie = new envoieCharactère(client, 9792);
+                    envoie.execute();
+                }
+            });
+
+            bouton_nani.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("envoie", "Bouton clic");
+                    envoieCharactère envoie = new envoieCharactère(client, 20309);
+                    envoie.execute();
+                }
+            });
+
+            bouton_brimestone.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("envoie", "Bouton clic");
+                    envoieCharactère envoie = new envoieCharactère(client, 128783);
+                    envoie.execute();
+                }
+            });
+        }catch(Exception e){
+            Log.i("exception", e.getMessage());
+        }
     }
 }
